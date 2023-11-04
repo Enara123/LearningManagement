850ba7a506680d86ee7828f6a978f67fa82af893
Shahein Ockersz <shaheinockersz1234@gmail.com>
2023-11-04 16:55:53 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
