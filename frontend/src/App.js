eba136618ea3676b8db0e0f5a4fbf56a780a6670
Shahein Ockersz <shaheinockersz1234@gmail.com>
2023-11-23 09:27:10 +0530

    New Front end

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ModuleCard from "./components/ModuleCard";
+
+function App() {
+  return (
+    <div className="App">
+      <ModuleCard moduleId="" />
+    </div>
+  );
+}
+
+export default App;
